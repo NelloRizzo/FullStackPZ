265db085ffda41786d9973242fc7429106c47a15
samueledibello <samuele.dibello00@gmail.com>
2025-05-07 16:35:25 +0200

    Aggiunto decoratori entità

--- src/main/java/corso/java/entities/Software_Entity.java
+++ src/main/java/corso/java/entities/Software_Entity.java
@@ -1,8 +1,10 @@
 package corso.java.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,11 +15,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "softwares")
 public class Software_Entity {
 	@Id
 	@GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
 	private Integer id;
+	@Column(name = "name", nullable = false, length = 50)
 	private String name;
+	@Column(name = "version", nullable = false, length = 50)
 	private String version;
 
 }
