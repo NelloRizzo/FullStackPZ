2447e760ec4eea40377207402f1249d7f3809397
samueledibello <samuele.dibello00@gmail.com>
2025-05-08 14:43:30 +0200

    Service eliminaSoftware per Id

--- front-end/src/app/components/softwares/softwarelist/softwarelist.component.ts
+++ front-end/src/app/components/softwares/softwarelist/softwarelist.component.ts
@@ -3,25 +3,27 @@ import { Software } from '../../../services/models';
 
 import { NgFor } from '@angular/common';
 import { SoftwareService } from '../../../services/software-services/software.service';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-softwarelist',
-  imports: [NgFor],
+  imports: [NgFor, FormsModule],
   templateUrl: './softwarelist.component.html',
   styleUrl: './softwarelist.component.css'
 })
 export class SoftwarelistComponent implements OnInit {
   softwares: Software[] = [];
-  constructor (private softwareService: SoftwareService) { }
+  idDaEliminare: number = 0;
+  constructor(private softwareService: SoftwareService) { }
 
-  ngOnInit() : void{
+  ngOnInit(): void {
     this.softwareService.getallsoftwares().subscribe(response => {
       this.softwares = response;
     })
   }
   cancellaSoftware(id: number) {
-    this.softwareService.cancellaSoftware(id).subscribe(() => {
-      this.softwares = this.softwares.filter(software => software.id !== id);
+    this.softwareService.cancellaSoftware(this.idDaEliminare).subscribe(() => {
+
     });
   }
 }
