c8c671e18af445d0452ee2154de19378da0ec540
RoccoDatena <roc87d@gmail.com>
2025-05-08 12:27:52 +0200

    Aggiunta CSS

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SoftwarelistComponent } from './components/softwares/softwarelist/softwarelist.component';
+import { AggiungiSoftwareComponent } from './components/softwares/aggiungi-software/aggiungi-software.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SoftwarelistComponent],
+  imports: [RouterOutlet, SoftwarelistComponent, AggiungiSoftwareComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
