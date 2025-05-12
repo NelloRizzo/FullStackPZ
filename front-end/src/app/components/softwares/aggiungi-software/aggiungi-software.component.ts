d64a97d678c120f6266e5ff30dd626f38ca4c23e
RoccoDatena <roc87d@gmail.com>
2025-05-12 12:10:15 +0200

    Aggiunta Root Navigate

--- front-end/src/app/components/softwares/aggiungi-software/aggiungi-software.component.ts
+++ front-end/src/app/components/softwares/aggiungi-software/aggiungi-software.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Software } from '../../../services/models';
 import { FormsModule } from '@angular/forms';
 import { SoftwareService } from '../../../services/software-services/software.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-aggiungi-software',
@@ -17,14 +18,16 @@ export class AggiungiSoftwareComponent {
     id: 0 // Assuming id is auto-generated and not needed for creation
   };
 
-  constructor(private softwareService: SoftwareService) {}
+  constructor(private softwareService: SoftwareService, private routes: Router) {}
 
   submit(): void {
     this.softwareService.generaSoftware(this.software).subscribe({
       next: () => {
         alert('Software inserito con successo!');
         this.software = { name: '', version: '' }; // reset
-      },
+        this.routes.navigate(['software/list']); // Navigate to the software list page after successful submission
+
+        },
       error: err => {
         console.error('Errore durante l’inserimento:', err);
         alert('Errore durante l’inserimento.');
