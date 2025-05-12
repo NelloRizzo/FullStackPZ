21fff1eb53762b0c8c7c73ab4158df641fbfbdc4
Zellowgram <eustachio.antonio1999@gmail.com>
2025-05-12 11:42:43 +0200

    Aggiunta routerLink a CompanyListComponent
    .

--- front-end/src/app/components/company/company-list/company-list.component.ts
+++ front-end/src/app/components/company/company-list/company-list.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CompanyServiceService } from '../../../services/company/company-service.service';
 import { Company } from '../../../services/company/company.model';
 import { NgFor, NgIf } from '@angular/common';
+import { AddCompanyComponent } from "../add-company/add-company.component";
+import { RouterLink, RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-company-list',
-  imports: [NgFor,NgIf],
+  imports: [NgFor, NgIf, RouterLink],
   templateUrl: './company-list.component.html',
   styleUrl: './company-list.component.css'
 })
